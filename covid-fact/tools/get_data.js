9bb3a3deba37f861632f623eed364654b25fa9ba
lon9 <rompei0924@gmail.com>
2020-03-12 13:50:22 +0900

    Modify get_data.

--- covid-fact/tools/get_data.js
+++ covid-fact/tools/get_data.js
@@ -1,51 +1,60 @@
 const fs = require('fs')
+const crypto = require('crypto')
 const axios = require('axios')
+const csvSync = require('csv-parse/lib/sync')
 
-const URL =
-  'https://vdata.nikkei.com/newsgraphics/coronavirus-world-map/data/data.tsv'
-
-function tsvJSON(tsv) {
-  const lines = tsv.split('\n')
-  const headers = lines.slice(0, 1)[0].split('\t')
-  return lines.slice(1, lines.length).map((line) => {
-    const data = line.split('\t')
-    return headers.reduce((obj, nextKey, index) => {
-      obj[nextKey] = data[index]
-      return obj
-    }, {})
-  })
-}
+const URLS = [
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+]
 
 async function main() {
-  const labels = []
-  const chinaCure = []
-  const deathRate = []
+  if (!fs.existsSync('static/data')) fs.mkdirSync('static/data')
+  let md5hash = crypto.createHash('md5')
+  md5hash.update('World')
+  const worldKey = md5hash.digest('hex')
+  const countries = {}
+  countries[worldKey] = {}
+  const countryNames = ['World']
 
-  try {
-    const res = await axios.get(URL)
-    const data = tsvJSON(res.data).slice(1)
-    data.forEach((elem) => {
-      labels.push(elem[''])
-      chinaCure.push({
-        day: elem[''],
-        infected: elem['中国本土の感染者数'],
-        cured: elem['中国本土の退院・治癒'],
-        rate:
-          Number(elem['中国本土の退院・治癒']) /
-          Number(elem['中国本土の感染者数'])
+  for (let i = 0; i < URLS.length; i++) {
+    try {
+      const res = await axios.get(URLS[i])
+      const kind = URLS[i].match(/.*-(.*)\.csv/)[1].toLowerCase()
+      const data = csvSync(res.data)
+      data.forEach((row, j) => {
+        if (i === 0 && j === 0) {
+          return fs.writeFileSync(
+            'static/data/labels.json',
+            JSON.stringify(row.slice(4))
+          )
+        } else if (j === 0) return
+        md5hash = crypto.createHash('md5')
+        md5hash.update(row[1], 'binary')
+        const country = md5hash.digest('hex')
+        const nums = row.slice(4)
+        if (!(country in countries)) {
+          countries[country] = {}
+          countryNames.push(row[1])
+        }
+        if (!(kind in countries[country]))
+          countries[country][kind] = new Array(nums.length).fill(0)
+        if (!(kind in countries[worldKey]))
+          countries[worldKey][kind] = new Array(nums.length).fill(0)
+        nums.forEach((num, k) => {
+          const n = Number(num)
+          countries[country][kind][k] += n
+          countries[worldKey][kind][k] += n
+        })
       })
-      deathRate.push({
-        day: elem[''],
-        infected: elem['世界全体の感染者'],
-        death: elem['世界の死者数'],
-        rate: Number(elem['世界の死者数']) / Number(elem['世界全体の感染者'])
-      })
-    })
-    fs.writeFileSync('data/labels.json', JSON.stringify(labels))
-    fs.writeFileSync('data/china_cure.json', JSON.stringify(chinaCure))
-    fs.writeFileSync('data/death_rate.json', JSON.stringify(deathRate))
-  } catch (err) {
-    console.error(err)
+    } catch (err) {
+      console.error(err)
+    }
+    for (const k in countries) {
+      fs.writeFileSync(`static/data/${k}.json`, JSON.stringify(countries[k]))
+    }
+    fs.writeFileSync('static/data/countries.json', JSON.stringify(countryNames))
   }
 }
 
