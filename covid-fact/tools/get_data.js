abeba680834003bdfdd7c224163c05f60c662c54
lon9 <rompei0924@gmail.com>
2020-03-27 22:26:49 +0900

    Update get_data

--- covid-fact/tools/get_data.js
+++ covid-fact/tools/get_data.js
@@ -4,9 +4,15 @@ const axios = require('axios')
 const csvSync = require('csv-parse/lib/sync')
 
 const URLS = [
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+]
+
+const KINDS = [
+  'Confirmed',
+  'Deaths',
+  'Recovered'
 ]
 
 async function main() {
@@ -21,7 +27,7 @@ async function main() {
   for (let i = 0; i < URLS.length; i++) {
     try {
       const res = await axios.get(URLS[i])
-      const kind = URLS[i].match(/.*-(.*)\.csv/)[1].toLowerCase()
+      const kind = KINDS[i]
       const data = csvSync(res.data)
       data.forEach((row, j) => {
         if (i === 0 && j === 0) {
